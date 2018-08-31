d6f1fabb548ca8c64b9d135f4b5ee0106f182cde
Robert DeLuca <robertdeluca19@gmail.com>
2018-08-31 15:28:06 -0500

    Fill in tests (#5)
    * Port cypress test suite the best I can
    
    Source: https://github.com/cypress-io/cypress-example-todomvc/blob/master/cypress/integration/app_spec.js
    
    * Remove network latency to match networkless cypress example
    
    * Use cli alpha
    
    * Update to latest bigtest cli

--- bigtest/interactors/todo-item.js
+++ bigtest/interactors/todo-item.js
@@ -1,10 +1,12 @@
 import {
-  interactor,
   text,
+  interactor,
   clickable,
   property,
   fillable,
-  triggerable
+  triggerable,
+  isPresent,
+  blurrable
 } from "@bigtest/interactor";
 
 @interactor
@@ -12,9 +14,17 @@ class TodoItem {
   todoText = text("label");
   toggle = clickable(".toggle");
   delete = clickable(".destroy");
+  toggleIsPresent = isPresent(".toggle");
+  deleteIsPresent = isPresent(".destroy");
   isCompleted = property(".toggle", "checked");
   doubleClick = triggerable("label", "dblclick");
   fillInput = fillable(".edit");
+  blurInput = blurrable(".edit");
+
+  pressEscape = triggerable(".edit", "keydown", {
+    keyCode: 27
+  });
+
   pressEnter = triggerable(".edit", "keydown", {
     keyCode: 13
   });
